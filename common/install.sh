f9c6d1b4b36517524eab46042f7307232b624b5b
JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>
2018-10-28 22:49:55 -0400

    Add files via upload

--- common/install.sh
+++ common/install.sh
@@ -1,10 +1,10 @@
 mkdir -p $UNITY$BINPATH
 cp_ch -n $INSTALLER/custom/dns_switch.sh $UNITY$BINPATH/dns_switch
-ui_print "Using $BINPATH."
-sed -i -e "s|<MAGISK>|$MAGISK|" -e "s|<BINPATH>|$BINPATH|" -e "s|<MODVERSION>|$(grep_prop versionCode $INSTALLER/module.prop)|" -e "s|<MODID>|$MODID|" $UNITY$BINPATH/dns_switch
+log_print "Using $BINPATH."
+sed -i -e "s|<MAGISK>|$MAGISK|" -e "s|<BINPATH>|$BINPATH|" -e "s|<MODVERSION>|$(grep_prop versionCode $INSTALLER/module.prop)|" -e "s|<MODID>|$MODID|" -e "s|<CACHELOC>|$CACHELOC|" $UNITY$BINPATH/dns_switch >> $INSTLOG 2>&1
 if $MAGISK; then
-sed -i -e "s|<PROP>|/sbin/.core/img/dns_switch/system.prop|" -e "s|<MODPATH>|/sbin/.core/img/dns_switch|" -e "s|<MODPROP>|$(echo $MOD_VER)|" $UNITY$BINPATH/dns_switch
+sed -i -e "s|<PROP>|/sbin/.core/img/dns_switch/system.prop|" -e "s|<MODPATH>|/sbin/.core/img/dns_switch|" -e "s|<MODPROP>|$(echo $MOD_VER)|" $UNITY$BINPATH/dns_switch >> $INSTLOG 2>&1
 else
-  sed -i -e "s|<PROP>|$PROP|" -e "s|<MODPROP>|$MOD_VER|" -e "s|<MODPATH>|\"\"|" $UNITY$BINPATH/dns_switch
+  sed -i -e "s|<PROP>|$PROP|" -e "s|<MODPROP>|$MOD_VER|" -e "s|<MODPATH>|\"\"|" $UNITY$BINPATH/dns_switch >> $INSTLOG 2>&1
 fi
-patch_script $UNITY$BINPATH/dns_switch
+patch_script $UNITY$BINPATH/dns_switch >> $INSTLOG 2>&1
